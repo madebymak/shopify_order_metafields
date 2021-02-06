e1ad9dbd3c7ed95f6e81df3b6f8a5d75503f7191
madebymak <mr.jmak@gmail.com>
2021-02-06 16:53:18 -0600

    Create new customer metafields

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ import express from 'express';
 // const path = require('path');
 import bodyParser from 'body-parser';
 
+import axios from 'axios';
+
+import dotenv from "dotenv";
+if (process.env.NODE_ENV !== 'production') {
+	dotenv.config();
+}
+
+const accessUrl = process.env.secret
 
 /**
  * App Variables
@@ -25,12 +33,44 @@ const PORT = 3000;
 app.use(bodyParser.json());
 
 // app.get('/', (req, res) => {
-// 	res.status(200).send('Hello World 132');
+// 	res.status(200).send('Hello World');
 // });
 
-app.post('/', (req, res) => { 
+app.post('/', (req, res) => {
 	res.send('POST');
-	console.log(req.body);
+	const order = req.body;
+	const orderId = order.id;
+
+	const testCustomerId = 4518786891927;
+	// console.log({ orderId });
+	// const customerUrl = `${accessUrl}/api/2021-01/customers.json`;
+	const customerUrl = `${accessUrl}/customers/${testCustomerId}/metafields.json`;
+
+
+	// /admin/customers/#{id}/metafields.json
+	const testMetafield = {
+		"metafield": {
+			"namespace": "add name",
+			"key": "add key",
+			"value": 'testing adding metafields',
+			"value_type": "string"
+		}
+	}
+
+	axios.post(customerUrl, testMetafield)
+		.then(res => console.log({res}))
+		.catch(error => console.log({error}))
+
+// get customer
+	// axios.get(customerUrl)
+	// 	.then((res) => { 
+	// 		console.log(res.data);
+	// 	})
+	// 	.catch(error => console.log({error}))
+
+
+	///admin/customers/#{id}/metafields.json
+	///admin/orders/#{id}/metafields.json
 })
 
 /**
