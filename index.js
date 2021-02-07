581b3f29fac6a0cc4db126395f28d5abd6505837
madebymak <mr.jmak@gmail.com>
2021-02-06 20:28:37 -0600

    Check if metafield already exists

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV !== 'production') {
 	dotenv.config();
 }
 
-const accessUrl = process.env.secret
+const accessUrl = process.env.shopify_secret;
 
 /**
  * App Variables
@@ -42,24 +42,45 @@ app.post('/', (req, res) => {
 	const orderId = order.id;
 
 	const testCustomerId = 4518786891927;
-	// console.log({ orderId });
-	// const customerUrl = `${accessUrl}/api/2021-01/customers.json`;
-	const customerUrl = `${accessUrl}/customers/${testCustomerId}/metafields.json`;
 
+	const customerUrl = `${accessUrl}/customers/${testCustomerId}/metafields.json`;
+	const orderUrl = `${accessUrl}/orders/${order.id}/metafields.json`;
+	// get order from webhook
+
+	// get customer id from order
+	// const customerId = order.customer.id;
+
+	// add metafield to customer
+	// /admin/orders/#{id}/metafields.json
 
 	// /admin/customers/#{id}/metafields.json
 	const testMetafield = {
 		"metafield": {
-			"namespace": "add name",
-			"key": "add key",
-			"value": 'testing adding metafields',
+			"namespace": "referral_email",
+			"key": "email",
+			"value": 'jon@email.com',
 			"value_type": "string"
 		}
 	}
 
-	axios.post(customerUrl, testMetafield)
-		.then(res => console.log({res}))
-		.catch(error => console.log({error}))
+	// check if metafield exists
+	axios.get(customerUrl)
+		.then((res) => {
+			const customerMetafieldList = res.data.metafields;
+			let referralExist = false;
+			// console.log({ customerMetafieldList });
+			customerMetafieldList.filter((metafield) => {
+				if (metafield.namespace == 'referral_email') {
+					referralExist = true;
+				}
+			});
+
+			// add referral metafield
+			// referralExist || addReferralEmail(customerUrl, testMetafield);
+			referralExist ? console.log('skipped') : addReferralEmail(customerUrl, testMetafield);
+		})
+		.catch(error => console.log({error}));
+
 
 // get customer
 	// axios.get(customerUrl)
@@ -80,4 +101,13 @@ app.post('/', (req, res) => {
 app.listen(PORT, () => {
 	console.log(`Listening on http://localhost:${PORT}`);
 });
+
+const addReferralEmail = (url, data) => {
+	console.log('bang');
+
+	axios.post(url, data)
+		// .then(res => console.log({ res }))
+		.then(() => console.log('added'))
+		.catch(error => console.log({error}))
+}
 
